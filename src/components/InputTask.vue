1da02c998c9b6ebd527a48a9cbc14f265ef319dc
BasilioTeam <junior.basilio@gmail.com>
2018-06-06 21:27:24 -0300

    inicial

---
+++ src/components/InputTask.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input class="new-todo"
+            placeholder="O que precisa ser feito?">
+    </div>
+</template>
+
+<script>
+import { Task } from '../models/Task'
+export default {
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
